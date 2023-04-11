546ad53740fbdcf59823f7b7fd263671d18d613d
Damien Mouchagues <damienmouchagues33@gmail.com>
2023-04-11 15:57:36 +0200

    Initialisation et ajout TP7 : ajout CompteTaux héritant de Compte

--- approche.objet/src/fr/diginamic/banque/entites/TestBanque.java
+++ approche.objet/src/fr/diginamic/banque/entites/TestBanque.java
@@ -13,6 +13,15 @@ public class TestBanque {
 		unCompte.crediter(1800);
 		System.out.println("Vous recevez votre salaire (+1800)");
 		System.out.println(unCompte.toString());
+		
+		Compte[] tabComptes = new Compte[2];
+		Compte compte1 = new Compte(2, 3204);
+		CompteTaux compte2 = new CompteTaux(3, 6495, 4);
+		tabComptes[0] = compte1;
+		tabComptes[1] = compte2;
+		for(Compte unCompteDansTab : tabComptes) {
+			System.out.println(unCompteDansTab.toString());
+		}
 	}
 	
 }
