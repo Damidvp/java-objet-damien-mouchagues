87c20f1c675f99f30a16a2c1d5dcb6c1ec1653ea
Damien Mouchagues <damienmouchagues33@gmail.com>
2023-04-13 16:44:46 +0200

    TP J3/J4 : modif exercice JDR -> ajout d'une créature puissante

---
+++ approche.objet/src/fr/diginamic/jdr/Bahamut.java
@@ -0,0 +1,9 @@
+package fr.diginamic.jdr;
+
+public class Bahamut extends Creature{
+
+	public Bahamut() {
+		super((int)(Math.random()*6)+15, (int)(Math.random()*6)+50);
+	}
+
+}
--- approche.objet/src/fr/diginamic/jdr/Jdr.java
+++ approche.objet/src/fr/diginamic/jdr/Jdr.java
@@ -6,84 +6,97 @@ public class Jdr {
 
 	public static void main(String[] args) {
 		boolean continueToPlay = true;
+		Personnage monPersonnage = new Personnage();
+		monPersonnage.afficherPersonnage();
 		while(continueToPlay) {
-			play(new Personnage());
 			Scanner scanner = new Scanner(System.in);
 			boolean choiceToContinueValide = false;
 			while(!choiceToContinueValide) {
-				System.out.println("Souhaitez-vous créer un autre personnage (y/n) ?");
+				afficherMenu();
 				switch(scanner.nextLine()) {
-				case "y":
-					System.out.println();
-					choiceToContinueValide = true;
-					break;
-				case "n":
-					choiceToContinueValide = true;
-					continueToPlay = false;
-					System.out.println();
-					System.out.println("*** Vous quittez le jeu, au revoir ! ***");
-					break;
-				default:
-					System.out.println("Erreur : renseignez 'y' ou 'n' pour continuer ou arrêter le jeu");
-					break;
+					case "1":
+						choiceToContinueValide = true;
+						monPersonnage = new Personnage();
+						System.out.println("--- NOUVEAU PERSONNAGE ---");
+						monPersonnage.afficherPersonnage();
+						break;
+					case "2":
+						if(monPersonnage.getPv() > 0) {
+							choiceToContinueValide = true;
+							play(monPersonnage);
+						} else {
+							System.out.println("Votre personnage est décédé. Votre score final est de " + monPersonnage.getScore() +
+									" points. Merci de créer un nouveau personnage.");
+							System.out.println();
+						}
+						break;
+					case "3":
+						afficherScore(monPersonnage);
+						break;
+					case "4":
+						choiceToContinueValide = true;
+						continueToPlay = false;
+						System.out.println("*** Vous quittez le jeu, au revoir ! ***");
+						break;
+					default:
+						System.out.println("Erreur : saisie non valide");
+						break;
 				}
 			}
 		}
 	}
 	
 	public static void play(Personnage personnage) {
-		System.out.println("*** DEBUT DU JEU ***");
-		System.out.println();
-		personnage.afficherPersonnage();
 		
 		//Le jeu continue tant que le personnage a des pv
-		while(personnage.getPv() > 0) {
-			Scanner scanner = new Scanner(System.in);
-			boolean ennemiChoisiValide = false;
-			Creature ennemi = null;
-			while(!ennemiChoisiValide) {
-				System.out.println("Choisissez une créature à combattre (1: Loup, 2: Gobelin, 3: Troll) : ");
-				switch(scanner.nextLine()) {
-				case "1":
-					ennemi = new Loup();
-					ennemiChoisiValide = true;
-					System.out.println();
-					break;
-				case "2":
-					ennemi = new Gobelin();
-					ennemiChoisiValide = true;
-					System.out.println();
-					break;
-				case "3":
-					ennemi = new Troll();
-					ennemiChoisiValide = true;
-					System.out.println();
-					break;
-				default:
-					System.out.println("Erreur : valeur entrée non valide");
-				}
-			}
-			int numTour = 1;
-			while((ennemi.getPv() > 0) && (personnage.getPv() > 0)) {
-				System.out.println("--- Tour " + numTour + " ---");
-				tour(ennemi, personnage);
-				numTour++;
-			}
-			System.out.println("--- FIN DU COMBAT ---");
-			if(ennemi.getPv() <= 0) {
-				System.out.println("!!! Créature vaincue (+" + getScoreGagne(ennemi) + " points) !!!");
+		Scanner scanner = new Scanner(System.in);
+		boolean ennemiChoisiValide = false;
+		Creature ennemi = null;
+		while(!ennemiChoisiValide) {
+			System.out.println("Choisissez une créature à combattre (1: Loup, 2: Gobelin, 3: Troll, 4: Bahamut) : ");
+			switch(scanner.nextLine()) {
+			case "1":
+				ennemi = new Loup();
+				ennemiChoisiValide = true;
 				System.out.println();
-				personnage.setScore(personnage.getScore() + getScoreGagne(ennemi));
-			}
-			if(personnage.getPv() <= 0) {
-				System.out.println("!!! Vous avez été vaincu !!!");
+				break;
+			case "2":
+				ennemi = new Gobelin();
+				ennemiChoisiValide = true;
 				System.out.println();
+				break;
+			case "3":
+				ennemi = new Troll();
+				ennemiChoisiValide = true;
+				System.out.println();
+				break;
+			case "4":
+				ennemi = new Bahamut();
+				ennemiChoisiValide = true;
+				System.out.println();
+				break;
+			default:
+				System.out.println("Erreur : valeur entrée non valide");
 			}
 		}
-		
-		System.out.println("*** GAME OVER ***");
-		System.out.println("Votre score : " + personnage.getScore());
+		System.out.println("*** DEBUT DU COMBAT ***");
 		System.out.println();
+		int numTour = 1;
+		while((ennemi.getPv() > 0) && (personnage.getPv() > 0)) {
+			System.out.println("--- Tour " + numTour + " ---");
+			tour(ennemi, personnage);
+			numTour++;
+		}
+		System.out.println("*** FIN DU COMBAT ***");
+		if(ennemi.getPv() <= 0) {
+			System.out.println("!!! Créature vaincue (+" + getScoreGagne(ennemi) + " points) !!!");
+			System.out.println();
+			personnage.setScore(personnage.getScore() + getScoreGagne(ennemi));
+		}
+		if(personnage.getPv() <= 0) {
+			System.out.println("!!! Vous avez été vaincu !!!");
+			System.out.println();
+		}
 	}
 	
 	public static void tour(Creature uneCreature, Personnage unPersonnage) {
@@ -116,6 +129,20 @@ public class Jdr {
 				break;
 		}
 		return score;
+	}
+	
+	public static void afficherScore(Personnage unPersonnage) {
+		System.out.println("Votre score actuel : " + unPersonnage.getScore());
+		System.out.println();
+	}
+	
+	public static void afficherMenu() {
+		System.out.println("*** MENU ***");
+		System.out.println("1. Créer un nouveau personnage");
+		System.out.println("2. Combattre une créature");
+		System.out.println("3. Afficher votre score actuel");
+		System.out.println("4. Quitter");
+		System.out.println();
 	}
 
 }
