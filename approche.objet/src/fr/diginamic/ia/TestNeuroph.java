c3f0ef8b67fa477de57104f0201d28cba1bdeed1
Damidvp <damienmouchagues33@gmail.com>
2023-04-18 15:56:50 +0200

    Ajout TP Réseau Neuronal : Simulation comportement OR

--- approche.objet/src/fr/diginamic/ia/TestNeuroph.java
+++ approche.objet/src/fr/diginamic/ia/TestNeuroph.java
@@ -11,30 +11,47 @@ public class TestNeuroph {
 
 	public static void main(String[] args) {
 		
-		MultiLayerPerceptron multiLayer = new MultiLayerPerceptron(TransferFunctionType.TANH, 2, 1, 1);
+		MultiLayerPerceptron multiLayer = new MultiLayerPerceptron(TransferFunctionType.TANH, 2, 4, 1);
+		
 		DataSet dataSet = new DataSet(2, 1);
 		DataSetRow rowOne = new DataSetRow(new double[] {0, 0}, new double[] {0});
 		DataSetRow rowTwo = new DataSetRow(new double[] {0, 1}, new double[] {1});
 		DataSetRow rowThree = new DataSetRow(new double[] {1, 0}, new double[] {1});
 		DataSetRow rowFour = new DataSetRow(new double[] {1, 1}, new double[] {1});
+		
 		dataSet.add(rowOne);
 		dataSet.add(rowTwo);
 		dataSet.add(rowThree);
 		dataSet.add(rowFour);
 		
+		int essais = 0;
+		boolean precisionOk = false;
 		multiLayer.learn(dataSet);
+		while(!precisionOk) {
+			multiLayer.learn(dataSet);
+			precisionOk = testIA(multiLayer, dataSet);
+			essais++;
+		}
 		
-		testIA(multiLayer, dataSet);
+		System.out.println("Apprentissage terminé en " + essais + " essais");
 	}
 	
-	public static void testIA(MultiLayerPerceptron layer, DataSet testSet) {
+	public static boolean testIA(MultiLayerPerceptron layer, DataSet testSet) {
+		boolean retour = false;
 		for(DataSetRow dataRow : testSet.getRows()) {
 			layer.setInput(dataRow.getInput());
 			layer.calculate();
 			double[] output = layer.getOutput();
 			System.out.println("Input : " + Arrays.toString(dataRow.getInput()));
 			System.out.println("Output : " + Arrays.toString(output));
+			for(double unOutput : output) {
+				if(unOutput < 0.0001) {
+					retour = true;
+				}
+			}
+			System.out.println();
 		}
+		return retour;
 	}
 
 }
