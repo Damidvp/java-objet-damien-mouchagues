2130ce626284f4002b0190433f0101dd1915cb41
Damien Mouchagues <damienmouchagues33@gmail.com>
2023-04-19 14:51:03 +0200

    Modif TP1 Exceptions : equals Ville compare aussi sur nb habitants

--- approche.objet/src/listes/Ville.java
+++ approche.objet/src/listes/Ville.java
@@ -47,6 +47,6 @@ public class Ville {
 			return false;
 		}
 		Ville autre = (Ville) object;
-		return this.nom.equals(autre.getNom());
+		return (this.nom.equals(autre.getNom()) && (this.nbHabitants == autre.getNbHabitants()));
 	}
 }
