21ac068de523b4b4a9a64370b44a1d733115874e
Damien Mouchagues <damienmouchagues33@gmail.com>
2023-04-19 14:49:38 +0200

    Modif TP1 Exception : modifications sur méthode equals de Ville (listes)

--- approche.objet/src/listes/TestEquals.java
+++ approche.objet/src/listes/TestEquals.java
@@ -7,8 +7,9 @@ public class TestEquals {
 	public static void main(String[] args) {
 		Ville ville1 = new Ville("Montpellier", 400000, Continent.EUROPE);
 		Ville ville2 = new Ville("Montpellier", 400000, Continent.EUROPE);
-		
-		System.out.println("ville1 equals ville2 : " + ville1.equals(ville2)); // == retourne false systématiquement -> on doit faire la comparaison sur tous les attributs
+
+		System.out.println("ville1 equals ville2 : " + ville1.equals(ville2)); //Résultat redéfinition equels
+		System.out.println("ville1 == ville2 : " + (ville1 == ville2)); //Identiques mais retourne false
 	}
 
 }
--- approche.objet/src/listes/Ville.java
+++ approche.objet/src/listes/Ville.java
@@ -6,42 +6,47 @@ public class Ville {
 	private String nom;
 	private int nbHabitants;
 	private Continent continent;
-	
+
 	public Ville(String nom, int nbHabitants, Continent continent) {
 		this.nom = nom;
 		this.nbHabitants = nbHabitants;
 		this.continent = continent;
 	}
-	
+
 	public int getNbHabitants() {
 		return nbHabitants;
 	}
-	
+
 	public void setNbHabitants(int nbHabitants) {
 		this.nbHabitants = nbHabitants;
 	}
-	
+
 	public String getNom() {
 		return nom;
 	}
-	
+
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-	
+
 	public Continent getContinent() {
 		return continent;
 	}
-	
+
 	public void setContinent(Continent continent) {
 		this.continent = continent;
 	}
-	
+
+	@Override
 	public String toString() {
 		return this.nom + " (" + this.nbHabitants + " habitants) [" + this.continent.getLibelle() + "]";
 	}
-	
-	public boolean equals(Ville uneVille) {
-		return (this.nom.equals(uneVille.getNom()) && this.nbHabitants == uneVille.getNbHabitants());
+
+	public boolean equals(Object object) {
+		if(!(object instanceof Ville)) {
+			return false;
+		}
+		Ville autre = (Ville) object;
+		return this.nom.equals(autre.getNom());
 	}
 }
